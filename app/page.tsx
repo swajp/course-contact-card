fd98e179322ec05ae19c79e21e91a5687697da48
swajp <stejskalmira@icloud.com>
2024-05-15 18:08:26 +0200

    updated svg to jsx

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function HomePage() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="w-8 h-8 text-gray-600"
             >
